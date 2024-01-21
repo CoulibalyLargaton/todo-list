50dd62ba1933e48e107b4eaff5e4597bb20a5e5e
Coulibaly Largaton Guy Emmanuel <coulibalylargatonguyemmanuel@gmail.com>
2024-01-21 16:04:46 +0000

    modifier les fonctions sauvegarderDonnees, chargerDonnees et ajout de la fonction changerEtatTache

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const conteneurTache = document.getElementById("conteneur-taches")
 let id = 0
 
 //toutes les taches
-const taches = [
+let taches = [
 
 ]
 
@@ -30,14 +30,18 @@ form.addEventListener("submit", (e) => {
         //ajout de la tache au tableau de toutes les taches
         taches.push(nouvelleTache)
 
+        //sauvegarder la tache
+        sauvegarderDonnees()
+
         //afficher la nouvelle tache
-        ajouterUneTache(nouvelleTache)
+        afficherUneTache(nouvelleTache)
+        
 
         inputTache.value = ""
     }
 })
 
-function ajouterUneTache(tache) {
+function afficherUneTache(tache) {
     const li = document.createElement("li")
 
     const input = document.createElement("input")
@@ -51,6 +55,7 @@ function ajouterUneTache(tache) {
     label.setAttribute("for", tache.id)
 
     const button = document.createElement("button")
+    button.dataset.id = tache.id
     button.innerText = "X"
 
     //ajout de la tache au DOM
@@ -59,3 +64,54 @@ function ajouterUneTache(tache) {
     li.appendChild(button)
     conteneurTache.appendChild(li)
 }
+
+conteneurTache.addEventListener("click", (e) => {
+    const baliseCible = e.target.tagName
+    if(baliseCible === "INPUT") {
+        // console.log("input")
+
+        //récupérer l'id de la tache à modifier
+        const idTache = e.target.getAttribute("id")
+        changerEtatTache(idTache)
+    }else if(baliseCible === "BUTTON") {
+        console.log("X")
+    }
+})
+
+function changerEtatTache(idTache) {
+    //modifier la tache concernée dans le tableau
+    const tachesMAJ = taches.map(tache => {
+        if(tache.id === Number(idTache)) {
+            tache.termine =!tache.termine
+        }
+        return tache
+    })
+    
+    // console.log(tachesMAJ)
+    //MAJ le tableau des taches
+    taches = tachesMAJ
+    sauvegarderDonnees()
+}
+
+
+function sauvegarderDonnees() {
+    localStorage.setItem("taches", JSON.stringify(taches))
+    localStorage.setItem("id", JSON.stringify(id))
+}
+
+function chargerDonnees() {
+    const donneesExistes = localStorage.getItem("taches")
+    if(donneesExistes) {
+        taches = JSON.parse(donneesExistes)
+        id = JSON.parse(localStorage.getItem("id"))
+        afficherTaches(taches)
+    }
+}
+
+function afficherTaches(taches) {
+    taches.forEach((tache) => {
+        afficherUneTache(tache)
+    })
+}
+
+chargerDonnees()
